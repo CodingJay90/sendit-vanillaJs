5b9a46fff5c52e1cc7e733710da9953ebc86127d
CodingJay90 <akoladefaruq42@gmail.com>
2020-12-27 15:51:02 +0100

    nav fix

--- scripts/logout.js
+++ scripts/logout.js
@@ -1,9 +1,28 @@
-const authBtn = document.querySelector(".auth");
+const authBtn = document.querySelectorAll(".auth");
 const logoutBtn = document.querySelector(".logout");
 const token = localStorage.getItem("token");
+const user = document.querySelector(".user");
 
 window.addEventListener("load", () => {
   if (token) {
-    authBtn.innerContent = "";
+    authBtn.forEach((i) => {
+      console.log();
+      i.parentElement.classList.add("none");
+    });
   }
+  fetch("http://localhost:5000/auth/user", {
+    method: "GET",
+    headers: {
+      "Content-type": "Apllication/json",
+      "x-access-token": token,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      user.innerHTML = data.first_name;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
